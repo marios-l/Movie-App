15a50baa32b78c6f12ef3bdd8de84d9147bf1061
marios-dev <marios-l-@hotmail.com>
2025-09-11 20:38:32 +0300

    husky + lint-staged

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-console.log('TMDB KEY:', import.meta.env.VITE_TMDB_API_KEY);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
